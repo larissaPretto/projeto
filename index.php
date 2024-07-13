a9d584e8faabd1a54833e8aa2d9d9a5206f4acaa
Enzo Zanini Becker <ezbecker@inf.ufsm.br>
2024-07-13 12:57:04 -0300

    estiliza caderninho

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>RadioEscape | Login</title>
+  <title>SpecHunter | Login</title>
   <link rel="icon" href="assets/icon.png">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
   <link rel="stylesheet" href="view/css/login.css">
